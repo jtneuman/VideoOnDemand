6a1b8b0c45daabaa8564d9dd31f78f11a362c4d9
Jeff Neumann <Jeff.Neumann@phoenix.edu>
2017-07-17 17:39:12 -0400

    Intial commit - Chapter 11 complete.

--- VideoOnDemand/Controllers/HomeController.cs
+++ VideoOnDemand/Controllers/HomeController.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using VideoOnDemand.Models;
 
 namespace VideoOnDemand.Controllers
 {
     public class HomeController : Controller
     {
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public HomeController(SignInManager<ApplicationUser> signInMgr)
+        {
+            _signInManager = signInMgr;
+        }
         public IActionResult Index()
         {
+            //check if user is signed in and redirect to login in AcctContr if anonymous; otherwise open default view
+            if (!_signInManager.IsSignedIn(User))
+                return RedirectToAction("Login", "Account");
+
             return View();
         }
 
