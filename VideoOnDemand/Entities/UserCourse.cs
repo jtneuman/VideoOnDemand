83f190cc77ccc30a7539fac37c6d246902804f6f
Jeff Neumann <Jeff.Neumann@phoenix.edu>
2017-07-18 17:07:06 -0400

    IRepo built and mock repo seed data entered. Stopped at GetCoursesMethod in Chapter 16, page 228.

--- VideoOnDemand/Entities/UserCourse.cs
+++ VideoOnDemand/Entities/UserCourse.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using VideoOnDemand.Models;
 
+// propbably need to fix the namespaces using for vod.models...
+
 namespace VideoOnDemand.Entities
 {
     public class UserCourse
