f129a746d79289916cdc5e7475d03e9714e2c274
Jeff Neumann <Jeff.Neumann@phoenix.edu>
2017-08-15 19:21:38 -0400

    Custon TagHelper class built - next URL parameters.

---
+++ VideoOnDemand/Tag Helpers/ButtonContainerTagHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace VideoOnDemand.Tag_Helpers
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("button-container")]
+    public class ButtonContainerTagHelper : TagHelper
+    {
+        public string Controller { get; set; }
+
+        public string Actions { get; set; }
+        public string Descriptions { get; set; }
+
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+            base.Process(context, output);
+
+            output.TagName = "span"; // Replaces <button-container> with <span> tag
+            output.Content.SetHtmlContent("<span style='min-width:100px; display: inline-block;'>");
+            var href = "";
+            var actions = Actions.Split(',');
+            var descriptions = Descriptions != null ? Descriptions.Split(',') : new string[0];
+            foreach (var action in actions)
+            {
+                if (Controller != null && Controller.Length > 0)
+                {
+                    if (action != null && action.Length > 0)
+                        href = $@"href='/admin/{Controller}/{action}'";
+                    else
+                        href = $@"href='/admin/{Controller}/Index'";
+                }
+
+                var description = action;
+                if (descriptions.Length >= actions.Length)
+                    description = descriptions[Array.IndexOf(actions, action)];
+
+                if (description.Length.Equals(0)) description = action;
+
+                output.Content.AppendHtml($@"<a {href}>{description}</a>");
+            }
+            // adding closing span tag
+            output.Content.AppendHtml("</span>");
+
+        }
+
+
+    }
+}
