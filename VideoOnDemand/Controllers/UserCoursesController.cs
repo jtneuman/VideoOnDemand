0c73c76325aad4a55434469cac46d8a299267e00
Jeff Neumann <Jeff.Neumann@phoenix.edu>
2017-08-14 20:45:31 -0400

    UserCoursesController with Index, Details and Create Get actions complete - next create post method.

---
+++ VideoOnDemand/Controllers/UserCoursesController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VideoOnDemand.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using VideoOnDemand.Models;
+using VideoOnDemand.Models.DTOModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VideoOnDemand.Controllers
+{
+    public class UserCoursesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserStore<ApplicationUser, IdentityRole, ApplicationDbContext> _userStore;
+
+        public UserCoursesController(UserStore<ApplicationUser, IdentityRole, ApplicationDbContext> userStore)
+        {
+            _db = userStore.Context;
+            _userStore = userStore;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var model = _db.Courses.Join(_db.UserCourses, c => c.Id, uc => uc.CourseId,
+                (c, uc) => new { Courses = c, UserCourses = uc }).Select(s => new UserCourseDTO
+                {
+                    CourseId = s.Courses.Id,
+                    CourseTitle = s.Courses.Title,
+                    UserId = s.UserCourses.UserId,
+                    UserEmail = _userStore.Users.FirstOrDefault(
+                        u => u.Id.Equals(s.UserCourses.UserId)).Email
+                });
+            return View(model);
+        }
+
+        public async Task<IActionResult> Details(string userId, int courseId)
+        {
+            if (userId == null || courseId.Equals(default(int)))
+            {
+                return NotFound();
+            }
+            var model = await _db.Courses.Join(_db.UserCourses, c => c.Id, uc => uc.CourseId,
+                (c, uc) => new { Courses = c, UserCourses = uc }).Select(s => new UserCourseDTO
+                {
+                    CourseId = s.Courses.Id,
+                    CourseTitle = s.Courses.Title,
+                    UserId = s.UserCourses.UserId,
+                    UserEmail = _userStore.Users.FirstOrDefault(
+                        u => u.Id.Equals(s.UserCourses.UserId)).Email
+                }).FirstOrDefaultAsync(w => w.CourseId.Equals(courseId) &&
+                        w.UserId.Equals(userId));
+
+            if (model == null)
+            {
+                return NotFound();
+            }
+            return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["CourseId"] = new SelectList(_db.Courses, "Id", "Title");
+            ViewData["UserId"] = new SelectList(_userStore.Users, "Id", "Email");
+
+            return View();
+        }
+    }
+}
